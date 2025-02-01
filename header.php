01f2b1f92d63e5ac002afeae9465da352e59393e
manimalladi <manimalladi05@gmail.com>
2025-02-01 17:47:02 +0530

    changes

---
+++ header.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>DiwaDental - Medical & Health HTML5 Template</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <!-- Favicon -->
+    <link
+      rel="shortcut icon"
+      type="image/x-icon"
+      href="assets/images/favicon.png"
+    />
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/font-awesome.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins/remixicon.css" />
+    <link rel="stylesheet" href="assets/css/plugins/flaticon.css" />
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins/animate.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins/slick.css" />
+    <link rel="stylesheet" href="assets/css/plugins/nice-select.css" />
+    <link rel="stylesheet" href="assets/css/plugins/gijgo.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins/preloader.css" />
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css" />
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+
+  <body>
+    <!--====== PRELOADER PART START ======-->
+
+    <div class="preloader">
+      <div class="loader">
+        <div class="ytp-spinner">
+          <div class="ytp-spinner-container">
+            <div class="ytp-spinner-rotator">
+              <div class="ytp-spinner-left">
+                <div class="ytp-spinner-circle"></div>
+              </div>
+              <div class="ytp-spinner-right">
+                <div class="ytp-spinner-circle"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--====== PRELOADER PART ENDS ======-->
+
+    <div class="main-wrapper">
+      <!-- Header Section Start -->
+      <div class="section header-section transparent-header">
+        <!-- Header Top Start -->
+        <div class="header-top d-none d-lg-block">
+          <div class="container">
+            <!-- Header Top Wrapper Start -->
+            <div class="header-top-wrapper">
+              <!-- Header Info Start  -->
+              <div class="header-top-info">
+                <ul>
+                  <li>
+                    <i class="ri-map-pin-add-line"></i>
+                    <span>751 North Berkshire Rd. Auburndale</span>
+                  </li>
+                  <li>
+                    <i class="ri-mail-unread-fill"></i>
+                    <span>info@example.com</span>
+                  </li>
+                  <li><i class="ri-phone-fill"></i> +123 4567 7899</li>
+                </ul>
+              </div>
+              <!-- Header Info End -->
+
+              <!-- Header Social Start -->
+              <div class="header-social">
+                <ul class="social">
+                  <li>
+                    <a href="#"><i class="ri-facebook-fill"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ri-twitter-fill"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ri-linkedin-fill"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ri-instagram-fill"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <!-- Header Social End -->
+            </div>
+            <!-- Header Top Wrapper End -->
+          </div>
+        </div>
+        <!-- Header Top End -->
+
+        <!-- Header Bottom Start -->
+        <div class="header-bottom">
+          <div class="container">
+            <!-- Header Bottom Wrapper Start -->
+            <div class="header-bottom-wrapper">
+              <!-- Header Logo Start -->
+              <div class="header-logo">
+                <a href="index.html"
+                  ><img src="assets/images/brand/logo.png" alt="Logo" style="width: 100px; style:100px;"
+                /></a>
+              </div>
+              <!-- Header logo End -->
+
+              <!-- Header Primary Menu Start -->
+              <div class="header-primary-menu d-none d-lg-block">
+                <ul class="nav-menu">
+                  <li><a class="active" href="index.html">Home</a></li>
+                  <li><a href="about.html">About</a></li>
+                  <li><a href="service.html">Services</a></li>
+                  <li><a href="blog.html">Blog</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                </ul>
+              </div>
+
+              <div class="header-btn d-none d-lg-block">
+                <a class="btn" href="appointment.html">Make Appointment</a>
+              </div>
+              <!-- Header Button End -->
+
+              <!-- Header Mobile Toggle Start -->
+              <div class="header-toggle d-lg-none">
+                <button
+                  class="menu-toggle"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#offcanvasMenu"
+                >
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
+              </div>
+              <!-- Header Mobile Toggle End -->
+            </div>
+            <!-- Header Bottom Wrapper End -->
+          </div>
+        </div>
+        <!-- Header Bottom End -->
+      </div>
