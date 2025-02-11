60c80198323a323a2ee78988cc701af878c9da84
manimalladi <manimalladi05@gmail.com>
2025-02-11 11:42:31 +0530

    changes updated

--- blog.php
+++ blog.php
@@ -197,27 +197,27 @@
       <a href="blogs.php?service="><button class="redirect_blog_srivice">All</button></a>
       <a href="blogs.php?service=Root Canal"><button class="redirect_blog_srivice">Root Canal</button></a>
       <a href="blogs.php?service=Teeth Braces"><button class="redirect_blog_srivice">Teeth Braces</button></a>
-      <a href="blogs.php?service=Pediatric Dentist"><button class="redirect_blog_srivice"> Pediatric Dentist</button></a>
-      <a href="blogs.php?service=Paedodontist Doctors"><button class="redirect_blog_srivice">Paedodontist Doctors </button></a>
+      <!-- <a href="blogs.php?service=Pediatric Dentist"><button class="redirect_blog_srivice"> Pediatric Dentist</button></a> -->
+      <!-- <a href="blogs.php?service=Paedodontist Doctors"><button class="redirect_blog_srivice">Paedodontist Doctors </button></a> -->
       <a href="blogs.php?service=Clear Aligners"><button class="redirect_blog_srivice"> Clear Aligners</button></a>
-      <a href="blogs.php?service=Laminate Veneers"><button class="redirect_blog_srivice">Laminate Veneers</button></a>
+      <!-- <a href="blogs.php?service=Laminate Veneers"><button class="redirect_blog_srivice">Laminate Veneers</button></a> -->
       <a href="blogs.php?service=Crown Bridge"><button class="redirect_blog_srivice">Crown & Bridge</button></a>
       <a href="blogs.php?service=Dental Implant"><button class="redirect_blog_srivice">Dental Implant</button></a>
       <a href="blogs.php?service=Dentures Treatments"><button class="redirect_blog_srivice">Dentures Treatments</button></a>
-      <a href="blogs.php?service=Invisalign"><button class="redirect_blog_srivice">Invisalign </button></a>
+      <!-- <a href="blogs.php?service=Invisalign"><button class="redirect_blog_srivice">Invisalign </button></a> -->
       <a href="blogs.php?service=Jaw Corrective"><button class="redirect_blog_srivice">Jaw Corrective</button></a>
-      <a href="blogs.php?service=Laser Gum"><button class="redirect_blog_srivice">Laser & Gum</button></a>
-      <a href="blogs.php?service=Smile Designing"><button class="redirect_blog_srivice">Smile Designing</button></a>
-      <a href="blogs.php?service=Smile Makeover"><button class="redirect_blog_srivice">Smile Makeover</button></a>
-      <a href="blogs.php?service=Teeth Alignment"><button class="redirect_blog_srivice"> Teeth Alignment</button></a>
-      <a href="blogs.php?service=Tooth Extraction"><button class="redirect_blog_srivice">Tooth Extraction</button></a>
-      <a href="blogs.php?service=Tooth Cleaning"><button class="redirect_blog_srivice">Tooth Cleaning</button></a>
-      <a href="blogs.php?service=Gum Depigment"><button class="redirect_blog_srivice">Gum Depigment</button></a>
+      <!-- <a href="blogs.php?service=Laser Gum"><button class="redirect_blog_srivice">Laser & Gum</button></a> -->
+      <!-- <a href="blogs.php?service=Smile Designing"><button class="redirect_blog_srivice">Smile Designing</button></a> -->
+      <!-- <a href="blogs.php?service=Smile Makeover"><button class="redirect_blog_srivice">Smile Makeover</button></a>
+      <a href="blogs.php?service=Teeth Alignment"><button class="redirect_blog_srivice"> Teeth Alignment</button></a> -->
+      <!-- <a href="blogs.php?service=Tooth Extraction"><button class="redirect_blog_srivice">Tooth Extraction</button></a> -->
+      <!-- <a href="blogs.php?service=Tooth Cleaning"><button class="redirect_blog_srivice">Tooth Cleaning</button></a>
+      <a href="blogs.php?service=Gum Depigment"><button class="redirect_blog_srivice">Gum Surgery</button></a>
       <a href="blogs.php?service=Teeth Whitening"><button class="redirect_blog_srivice">Teeth Whitening</button></a>
       <a href="blogs.php?service=Laser Gum Surgery"><button class="redirect_blog_srivice">Laser Gum Surgery</button></a>
       <a href="blogs.php?service=Mouth Ulcers"><button class="redirect_blog_srivice">Mouth Ulcers</button></a>
       <a href="blogs.php?service=Precancerous Lesion"><button class="redirect_blog_srivice">Precancerous Lesion</button></a>
-      <a href="blogs.php?service=Laser Crown Lengthening"><button class="redirect_blog_srivice">Laser Crown Lengthening</button></a>
+      <a href="blogs.php?service=Laser Crown Lengthening"><button class="redirect_blog_srivice">Laser Crown Lengthening</button></a> -->
     </div>
   </div>
 
--- header.php
+++ header.php
@@ -52,82 +52,83 @@
 <body>
   <!--====== PRELOADER PART START ======-->
   <section>
-  <nav class="navbar navbar-expand-lg navbar-light Navebar_content">
-    <div class="container">
-      <a class="navbar-brand" href="index.php">
-        <img src="assets/images/brand/logo_navbar.png" alt="Logo" class="img-fluid" />
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto align-items-lg-center">
-          <li class="nav-item">
-            <a class="nav-link active" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
-          </li>
+    <nav class="navbar navbar-expand-lg navbar-light Navebar_content">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">
+          <img src="assets/images/brand/logo_navbar.png" alt="Logo" class="img-fluid" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto align-items-lg-center">
+            <li class="nav-item">
+              <a class="nav-link active" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About</a>
+            </li>
 
-          <!-- Services Dropdown -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Services
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li class="dropdown-submenu">
-                <a class="dropdown-item" href="services.php">Dental Treatment</a>
-                <ul class="dropdown-menu submenu">
-                  <li><a class="dropdown-item" href="implants.php">Implants</a></li>
-                  <li><a class="dropdown-item" href="rootcanal.php">Root Canal</a></li>
-                  <li><a class="dropdown-item" href="tooth_aligners.php">Tooth Aligners</a></li>
-                  <li><a class="dropdown-item" href="bridges.php">Bridges</a></li>
-                  <li><a class="dropdown-item" href="gum_surgery.php">Gum Surgery</a></li>
-                  <li><a class="dropdown-item" href="maxillofacial_surgery.php">Maxillofacial Surgery</a></li>
-                  <li><a class="dropdown-item" href="lasers.php">Laser Treatment</a></li>
-                </ul>
-              </li>
-              <li class="dropdown-submenu">
-                <a class="dropdown-item" href="cosmetic_services.php">Cosmetic Services</a>
-                <ul class="dropdown-menu submenu">
-                  <li><a class="dropdown-item" href="teeth_whitening.php">Teeth Whitening</a></li>
-                  <li><a class="dropdown-item" href="veeners.php">Veneers</a></li>
-                  <li><a class="dropdown-item" href="composit_filler.php">Composite Filler</a></li>
-                  <li><a class="dropdown-item" href="teeth_jewellery.php">Teeth Jewellery</a></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
+            <!-- Services Dropdown -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Services
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li class="dropdown-submenu">
+                  <a class="dropdown-item" href="services.php">Dental Treatment</a>
+                  <ul class="dropdown-menu submenu">
+                    <li><a class="dropdown-item" href="implants.php">Implants</a></li>
+                    <li><a class="dropdown-item" href="rootcanal.php">Root Canal</a></li>
+                    <li><a class="dropdown-item" href="tooth_aligners.php">Tooth Aligners</a></li>
+                    <li><a class="dropdown-item" href="bridges.php">Bridges</a></li>
+                    <li><a class="dropdown-item" href="gum_surgery.php">Gum Surgery</a></li>
+                    <li><a class="dropdown-item" href="maxillofacial_surgery.php">Maxillofacial Surgery</a></li>
+                    <li><a class="dropdown-item" href="lasers.php">Laser Treatment</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown-submenu">
+                  <a class="dropdown-item" href="cosmetic_services.php">Cosmetic Services</a>
+                  <ul class="dropdown-menu submenu">
+                    <li><a class="dropdown-item" href="teeth_whitening.php">Teeth Whitening</a></li>
+                    <li><a class="dropdown-item" href="veneers.php">Veneers</a></li>
+                    <li><a class="dropdown-item" href="composite_filler.php">Composite Filler</a></li>
+                    <li><a class="dropdown-item" href="teeth_jewellery.php">Teeth Jewellery</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
 
-          <!-- Equipment Dropdown -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="equipmentDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Equipment
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="equipmentDropdown">
-              <li><a class="dropdown-item" href="dental_ct_scan.php">Dental CT-Scan</a></li>
-              <li><a class="dropdown-item" href="ray.php">3D X-Ray</a></li>
-              <li><a class="dropdown-item" href="lasers.php">Laser</a></li>
-            </ul>
-          </li>
+            <!-- Equipment Dropdown -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="equipmentDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Equipment
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="equipmentDropdown">
+                <li><a class="dropdown-item" href="dental_ct_scan.php">Dental CT-Scan</a></li>
+                <li><a class="dropdown-item" href="ray.php">3D X-Ray</a></li>
+                <li><a class="dropdown-item" href="lasers.php">Laser</a></li>
+              </ul>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="blog.php">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.php">Contact</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blog.php">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact</a>
+            </li>
 
-          <!-- Make Appointment Button (Aligned) -->
-          <li class="nav-item">
-            <a class="btn btn-primary nav-btn ms-lg-3" href="appointment.php">Make Appointment</a>
-          </li>
-        </ul>
+            <!-- Make Appointment Button (Aligned) -->
+            <li class="nav-item">
+              <a class="btn btn-primary nav-btn ms-lg-3" href="appointment.php">Make Appointment</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-</section>
+    </nav>
+  </section>
+
 
   <!-- Optional JavaScript (if needed) -->
   <script>
